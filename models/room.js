0f83a3dfa4cdb4edbe8ce2d16cf0841ac9d4cd40
fabio7621 <d6200121a@gmail.com>
2024-04-03 17:14:59 +0800

    POST

--- models/room.js
+++ models/room.js
@@ -5,22 +5,20 @@ const roomSchema = new mongoose.Schema(
     name: String,
     price: {
       type: Number,
-      require: [true, "價格必填"],
+      required: [true, "價格必填"],
     },
     rating: Number,
     createdAt: {
       type: Date,
       default: Date.now,
-      select: false, //可以在shell使用find的時候不顯示出來
+      select: false,
     },
   },
   {
     versionKey: false,
-    timestamps: true,
   }
 );
 
 const Room = mongoose.model("Room", roomSchema);
 
-module.exports = room;
-//把room modal 傳出去外面就能直接使用
+module.exports = Room;
--- sever.js
+++ sever.js
@@ -1,9 +1,7 @@
-const { error } = require("console");
 const http = require("http");
 const mongoose = require("mongoose");
-const { version } = require("os");
-
-//連接資料庫
+const Room = require("./models/room");
+// 連接資料庫
 mongoose
   .connect("mongodb://localhost:27017/hotel")
   .then(() => {
@@ -13,25 +11,52 @@ mongoose
     console.log(error);
   });
 
-//實例 實體    instance
-const testRoom = new Room({
-  name: "無敵海景雙人房",
-  price: 2070,
-  rating: 4.5,
-});
-//儲存
-testRoom
-  .save()
-  .then(() => {
-    console.log("新增資料成功");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
 
-const requestListener = (req, res) => {
-  console.log(req);
-  res.end();
+const requestListener =async (req, res) => {
+  let body= "";
+  req.on('data',chunk=>{
+  body+=chunk;
+})  
+  const headers = {
+    "Access-Control-Allow-Headers":
+    "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "PATCH, POST, GET,OPTIONS,DELETE",
+    "Content-Type": "application/json",
+  };
+  if (req.url == "/rooms" && req.method == "GET") {
+    const rooms =await Room.find();
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        "status": "success",
+        rooms,
+      })
+    );
+    res.end();
+  }else if(req.url == "/rooms" && req.method == "POST"){
+    req.on('end',async()=>{
+      try{
+         const data =JSON.parse(body);
+         const newRoom = await Room.create(
+          {
+            name:data.name,
+            price:data.price,
+            rooms:data.rating
+          }
+         )
+         res.writeHead(200,headers);
+         res.write(JSON.stringify({
+          "status": "success",
+          rooms:newRoom
+         }))
+         res.end();
+      }catch(error){
+         console.log(error);
+         res.end()
+      }
+    })
+  }
 };
 
 const server = http.createServer(requestListener);
